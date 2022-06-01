2d96fc262d7c3ecc2f9bd37a203eeb015acbe07b
Kjubie Bruckner <abru1322@gmail.com>
2022-06-01 15:15:17 +0200

    finished backend

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var crypto = require('crypto');
+var cors = require('cors')
+
 var app = express();
 
 var types = ["sha1", "md5", "sha256", "sha512"]
+
+app.use(cors())
 
 app.use(
   express.urlencoded({
@@ -17,13 +21,14 @@ app.get('/hash', (req, res) => {
 })
 
 app.post('/hash', (req, res) => {
+	console.log(crypto.createHash(req.body.type).update(req.body.text).digest('base64'));
 	if (!types.includes(req.body.type))
 		res.end("Invalid hash type!\nPlease choose one of the following : md5, sha1, sha256, sha512");
 	else
 		res.end(crypto.createHash(req.body.type).update(req.body.text).digest('base64'));
 })
 
-var server = app.listen(443, function () {
+var server = app.listen(8081, function () {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
