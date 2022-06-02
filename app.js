23261c6517376d7636459722991e412fd70b5706
kjubie <65290601+kjubie@users.noreply.github.com>
2022-06-02 16:21:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use(
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.end("Hello!\nPlease send a POST request to /hash with 'text:<textToHash>' and 'type:<hashingType>'!\nAllowed hashing types: md5, sha1, sha256, sha512");
+	res.end("Hello! \nPlease send a POST request to /hash with 'text:<textToHash>' and 'type:<hashingType>'!\nAllowed hashing types: md5, sha1, sha256, sha512");
 })
 
 app.post('/hash', (req, res) => {
 	if (!types.includes(req.body.type)) {
 		res.statusCode = 400;
-		res.end("Invalid hash type!\nPlease choose one of the following : md5, sha1, sha256, sha512");
+		res.end("Invalid hash type! \nPlease choose one of the following : md5, sha1, sha256, sha512");
 	} else {
 		res.end(crypto.createHash(req.body.type).update(req.body.text).digest('base64'));
 	}
