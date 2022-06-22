8edca8e4e36dfc576d5f3c981dc75f80fdc69e6b
Kjubie Bruckner <abru1322@gmail.com>
2022-06-22 12:56:54 +0200

    Updated log message

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/hash', (req, res) => {
 var server = app.listen(8081, function () {
 	var host = server.address().address
 	var port = server.address().port
-	console.log("Example app listening at http://%s:%s", host, port)
+	console.log("Backend app listening at http://%s:%s", host, port)
 })
 
 module.exports = server
